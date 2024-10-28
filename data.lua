872de7febc673b3085907361d333edc461908c8e
BinaryElement <33382750+BinaryElement@users.noreply.github.com>
2024-10-28 13:26:59 -0400

    Initial functionality

--- data.lua
+++ data.lua
@@ -1,20 +1,307 @@
 
-data.raw["recipe"]["molten-copper"].ingredients = {
-    {
-        amount = 50,
-        name = "copper-ore",
-        type = "item"
+-- General
+if settings.startup["sssa-unlock-all-qualities"].value then
+    data.raw["technology"]["epic-quality"].hidden = true
+    data.raw["technology"]["legendary-quality"].hidden = true
+    data.raw["technology"]["automation"].effects = {
+        {
+          recipe = "assembling-machine-1",
+          type = "unlock-recipe"
+        },
+        {
+          recipe = "long-handed-inserter",
+          type = "unlock-recipe"
+        },
+        {
+          quality = "uncommon",
+          type = "unlock-quality"
+        },
+        {
+          quality = "rare",
+          type = "unlock-quality"
+        },
+        {
+          quality = "epic",
+          type = "unlock-quality"
+        },
+        {
+          quality = "legendary",
+          type = "unlock-quality"
+        }
     }
-}
-data.raw["recipe"]["molten-iron"].ingredients = {
-    {
-        amount = 50,
-        name = "iron-ore",
-        type = "item"
+    data.raw["technology"]["quality-module"].effects = {
+        {
+          recipe = "quality-module",
+          type = "unlock-recipe"
+        }
+      }
+end
+if settings.startup["sssa-revert-module-recipes"].value then
+    data.raw.recipe["speed-module-3"].ingredients = {
+        {
+          amount = 4,
+          name = "speed-module-2",
+          type = "item"
+        },
+        {
+          amount = 5,
+          name = "advanced-circuit",
+          type = "item"
+        },
+        {
+          amount = 5,
+          name = "processing-unit",
+          type = "item"
+        }
     }
-}
+    data.raw.recipe["productivity-module-3"].ingredients = {
+        {
+          amount = 4,
+          name = "productivity-module-2",
+          type = "item"
+        },
+        {
+          amount = 5,
+          name = "advanced-circuit",
+          type = "item"
+        },
+        {
+          amount = 5,
+          name = "processing-unit",
+          type = "item"
+        }
+    }
+    data.raw.recipe["efficiency-module-3"].ingredients = {
+        {
+          amount = 4,
+          name = "efficiency-module-2",
+          type = "item"
+        },
+        {
+          amount = 5,
+          name = "advanced-circuit",
+          type = "item"
+        },
+        {
+          amount = 5,
+          name = "processing-unit",
+          type = "item"
+        }
+    }
+end
+if settings.startup["sssa-revert-cliff-explosive-recipe"].value then
+    data.raw.recipe["cliff-explosives"].ingredients = {
+        {
+          amount = 10,
+          name = "explosives",
+          type = "item"
+        },
+        {
+          amount = 1,
+          name = "grenade",
+          type = "item"
+        },
+        {
+          amount = 1,
+          name = "barrel",
+          type = "item"
+        }
+    }
+end
+if settings.startup["sssa-rocket-parts-per-rocket"].value ~= 50 then
+    data.raw["rocket-silo"]["rocket-silo"].rocket_parts_required = 10
+end
+if settings.startup["sssa-rockets-only-need-fuel"].value then
+    data.raw.recipe["rocket-part"].ingredients = {
+        {
+          amount = 1,
+          name = "rocket-fuel",
+          type = "item"
+        }
+    }
+end
+if settings.startup["sssa-easy-space-foundations"].value then
+    data.raw.item["space-platform-foundation"].stack_size = 200
+    data.raw.item["space-platform-foundation"].weight = 10000
+    data.raw.recipe["space-platform-foundation"].results = {
+        {
+            amount = 10,
+            name = "space-platform-foundation",
+            type = "item"
+        }
+    }
+end
+if settings.startup["sssa-easter-egg"].value then
+    local qm = table.deepcopy(data.raw.module["quality-module-3"])
+    qm.name = "quality-module-3-sssa"
+    qm.effect = {
+        quality = 100,
+        speed = 1.00
+    }
+    local qmsmelt = table.deepcopy(data.raw.recipe["iron-plate"])
+    qm.name = "quality-module-3-sssa"
+    qm.ingredients = {
+        {
+          amount = 1,
+          name = "quality-module-1",
+          type = "item"
+        }
+    }
+    qm.results = {
+        {
+          amount = 1,
+          name = "quality-module-3-sssa",
+          type = "item"
+        }
+    }
+    data:extend({qm, qmsmelt})
+end
 
-data.raw["turret"]["small-worm-turret"].attack_parameters.range = 35 --up from 25
-data.raw["turret"]["medium-worm-turret"].attack_parameters.range = 45 --up from 30
-data.raw["turret"]["big-worm-turret"].attack_parameters.range = 55 --up from 38
-data.raw["turret"]["behemoth-worm-turret"].attack_parameters.range = 65 --up from 48
+-- Space
+
+-- Nauvis
+
+-- Vulcanus
+if settings.startup["sssa-remove-vulcanus-location-requirements"].value then
+    data.raw.recipe["foundry"].surface_conditions = nil
+    data.raw.recipe["big-mining-drill"].surface_conditions = nil
+end
+if settings.startup["sssa-remove-calcite-from-melting-ores"].value then
+    data.raw.recipe["molten-copper"].ingredients = {
+        {
+            amount = 50,
+            name = "copper-ore",
+            type = "item"
+        }
+    }
+    data.raw.recipe["molten-iron"].ingredients = {
+        {
+            amount = 50,
+            name = "iron-ore",
+            type = "item"
+        }
+    }
+end
+if settings.startup["sssa-revert-artillery-recipes"].value then
+    data.raw.recipe["artillery-shell"].ingredients = {
+        {
+          amount = 1,
+          name = "radar",
+          type = "item"
+        },
+        {
+          amount = 4,
+          name = "explosive-cannon-shell",
+          type = "item"
+        },
+        {
+          amount = 8,
+          name = "explosives",
+          type = "item"
+        }
+    }
+    --[[data.raw.recipe["artillery-shell-recycling"].results = {
+        {
+            extra_count_fraction = 0.25,
+            amount = 1,
+            name = "radar",
+            type = "item"
+        },
+        {
+            extra_count_fraction = 0.25,
+            amount = 4,
+            name = "explosive-cannon-shell",
+            type = "item"
+        },
+        {
+            extra_count_fraction = 0.25,
+            amount = 8,
+            name = "explosives",
+            type = "item"
+        }
+    }]]
+    data.raw.recipe["artillery-turret"].ingredients = {
+        {
+            amount = 60,
+            name = "steel-plate",
+            type = "item"
+        },
+        {
+            amount = 60,
+            name = "concrete",
+            type = "item"
+        },
+        {
+            amount = 40,
+            name = "iron-gear-wheel",
+            type = "item"
+        },
+        {
+            amount = 20,
+            name = "advanced-circuit",
+            type = "item"
+        }
+    }
+    data.raw.recipe["artillery-wagon"].ingredients = {
+        {
+            amount = 64,
+            name = "engine-unit",
+            type = "item"
+        },
+        {
+            amount = 10,
+            name = "iron-gear-wheel",
+            type = "item"
+        },
+        {
+            amount = 40,
+            name = "steel-plate",
+            type = "item"
+        },
+        {
+            amount = 16,
+            name = "pipe",
+            type = "item"
+        },
+        {
+            amount = 20,
+            name = "advanced-circuit",
+            type = "item"
+        }
+    }
+end
+
+-- Fulgora
+if settings.startup["sssa-remove-fulgora-location-requirements"].value then
+    data.raw.recipe["electromagnetic-plant"].surface_conditions = nil
+    data.raw.recipe["recycler"].surface_conditions = nil
+end
+if settings.startup["sssa-remove-stone-from-liquid-recipes"].value then
+    data.raw.recipe["electrolyte"].ingredients = {
+        {
+          amount = 10,
+          name = "heavy-oil",
+          type = "fluid"
+        },
+        {
+          amount = 10,
+          name = "holmium-solution",
+          type = "fluid"
+        }
+    }
+    data.raw.recipe["holmium-solution"].ingredients = {
+        {
+          amount = 2,
+          name = "holmium-ore",
+          type = "item"
+        },
+        {
+          amount = 10,
+          name = "water",
+          type = "fluid"
+        }
+    }
+end
+
+-- Gleba
+-- Aquilo
---
+++ settings.lua
@@ -0,0 +1,95 @@
+data:extend({
+    -- General settings a
+    {
+        type = "bool-setting",
+        name = "sssa-unlock-all-qualities",
+        setting_type = "startup",
+        default_value = true,
+        order = "aaa"
+    },
+    {
+        type = "bool-setting",
+        name = "sssa-revert-module-recipes",
+        setting_type = "startup",
+        default_value = true,
+        order = "aab"
+    },
+    {
+        type = "bool-setting",
+        name = "sssa-revert-cliff-explosive-recipe",
+        setting_type = "startup",
+        default_value = true,
+        order = "aac"
+    },
+    {
+        type = "int-setting",
+        name = "sssa-rocket-parts-per-rocket",
+        setting_type = "startup",
+        default_value = 10,
+        minimum_value = 1,
+        maximum_value = 1000,
+        order = "aad"
+    },
+    {
+        type = "bool-setting",
+        name = "sssa-rockets-only-need-fuel",
+        setting_type = "startup",
+        default_value = false,
+        order = "aae"
+    },
+    {
+        type = "bool-setting",
+        name = "sssa-easy-space-foundations",
+        setting_type = "startup",
+        default_value = true,
+        order = "aae"
+    },
+    {
+        type = "bool-setting",
+        name = "sssa-easter-egg",
+        setting_type = "startup",
+        default_value = true,
+        order = "azz"
+    },
+    -- Nauvis Settings b
+    -- Space Settings c
+    -- Vulcanus Settings d
+    {
+        type = "bool-setting",
+        name = "sssa-remove-vulcanus-location-requirements",
+        setting_type = "startup",
+        default_value = true,
+        order = "daa"
+    },
+    {
+        type = "bool-setting",
+        name = "sssa-remove-calcite-from-melting-ores",
+        setting_type = "startup",
+        default_value = true,
+        order = "dab"
+    },
+    {
+        type = "bool-setting",
+        name = "sssa-revert-artillery-recipes",
+        setting_type = "startup",
+        default_value = true,
+        order = "dac"
+    },
+    -- Fulgora Settings e
+    {
+        type = "bool-setting",
+        name = "sssa-remove-fulgora-location-requirements",
+        setting_type = "startup",
+        default_value = true,
+        order = "eaa"
+    },
+    {
+        type = "bool-setting",
+        name = "sssa-remove-stone-from-liquid-recipes",
+        setting_type = "startup",
+        default_value = false,
+        order = "eab"
+    },
+    -- Gleba Settings f
+    -- Aquilo Settings g
+})
