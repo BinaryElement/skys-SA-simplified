611565a02817ead33d560fc68c84862c9e73ac5b
BinaryElement <33382750+BinaryElement@users.noreply.github.com>
2024-10-28 11:39:32 -0400

    Initial commit

---
+++ data.lua
@@ -0,0 +1,20 @@
+
+data.raw["recipe"]["molten-copper"].ingredients = {
+    {
+        amount = 50,
+        name = "copper-ore",
+        type = "item"
+    }
+}
+data.raw["recipe"]["molten-iron"].ingredients = {
+    {
+        amount = 50,
+        name = "iron-ore",
+        type = "item"
+    }
+}
+
+data.raw["turret"]["small-worm-turret"].attack_parameters.range = 35 --up from 25
+data.raw["turret"]["medium-worm-turret"].attack_parameters.range = 45 --up from 30
+data.raw["turret"]["big-worm-turret"].attack_parameters.range = 55 --up from 38
+data.raw["turret"]["behemoth-worm-turret"].attack_parameters.range = 65 --up from 48
